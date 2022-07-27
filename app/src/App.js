8127e60c2cfe456f6618cb422b98ac608e76cf9e
carlos <kalelcarlosmartinez@gmail.com>
2022-07-27 11:32:44 -0700

    adding routes with react-router-dom

--- app/src/App.js
+++ app/src/App.js
@@ -1,30 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import List from "./components/Lists/list.component";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
-  return <List />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="*" name="Home" element={<List />}></Route>
+      </Routes>
+    </HashRouter>
+  );
 };
 
 export default App;
